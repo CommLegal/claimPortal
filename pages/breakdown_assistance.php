582d10fc34eb3fcb720980ad15cb9ebb9b29e9a3
unknown <js9@OC-WS-576-PC.onecall.local>
2015-11-27 23:03:39 +0000

    a

--- pages/breakdown_assistance.php
+++ pages/breakdown_assistance.php
@@ -3,10 +3,13 @@
 <div class="col-md-12 mb10">              
     <h3>Breakdown Assistance</h3><div class="title-divider"></div>
     <p>For more information regarding our policies or terms and conditions please download and inspect the documents below.</p>
-    <div class="btn-group" role="group" aria-label="...">
-      <a href="<?php echo _ROOT ?>/pdf-docs/breakdown-terms-of-business-agreement.pdf"><button type="button" class="btn btn-default">Breakdown Terms of Business Agreement</button></a>
-      <a href="<?php echo _ROOT ?>/pdf-docs/one-call-insurance-services-breakdown.pdf"><button type="button" class="btn btn-default">OneCall Insurance Breakdown Services</button></a>
-      <a href="<?php echo _ROOT ?>/pdf-docs/frequently-asked-questions-breakdown.pdf"><button type="button" class="btn btn-default">Frequently Asked Questions</button></a>
+    <div class="well"<
+        <div class="btn-group" role="group" aria-label="...">
+          <a href="<?php echo _ROOT ?>/pdf-docs/breakdown-terms-of-business-agreement.pdf" target="_blank"><button type="button" class="btn btn-default">Breakdown Terms of Business Agreement</button></a>
+          <a href="<?php echo _ROOT ?>/pdf-docs/one-call-insurance-services-breakdown.pdf" target="_blank"><button type="button" class="btn btn-default">OneCall Insurance Breakdown Services</button></a>
+          <a href="<?php echo _ROOT ?>/pdf-docs/frequently-asked-questions-breakdown.pdf" target="_blank"><button type="button" class="btn btn-default">Frequently Asked Questions</button></a>
+        </div>
+        <div style="clear: both;"</div>
     </div>
 </div> 
 
