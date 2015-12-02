ef9f4ea91011effda7cfb376344410912ac581ad
unknown <js9@OC-WS-576-PC.onecall.local>
2015-12-02 11:29:02 +0000

    a

--- functions/nci_form_log.php
+++ functions/nci_form_log.php
@@ -1,8 +1,6 @@
 <?php
 require("../includes/mysqlwrapper_class.php");
 $conn = new mysqlwrapper_class;
-
-var_dump($_POST);
 	
 $conn->execute_sql("insert", array("np_p_id" => $_POST['p_id'], "np_postValues" => $_POST['href'], "np_timestamp" => date("Y-m-d H:i:s")), "nci_post", "", "");
 
