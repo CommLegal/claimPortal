98f2b9d19200d39fee24c7e4a0f7dfafc5a26e77
unknown <js9@OC-WS-576-PC.onecall.local>
2015-12-03 15:17:17 +0000

    a

--- pages/search.php
+++ pages/search.php
@@ -197,7 +197,7 @@
                     	<?php
 						$date = new DateTime(date("Y-m-d"));
 						$date->sub(new DateInterval('P5D'));
-						$last_week = $date->format('Ymd');
+						$last_week = $date->format('dmY');
 				
 						$previousBreakdowns = $conn->execute_sql("select", array('c_id, c_timestamp, bd_assisted_unassisted, bd_further_info, c_ul_id', 'bd_id'), "claims JOIN breakdown_assistance ON c_bd_id = bd_id", "c_p_id = ?", array("i" => $policyDetail[0]['p_id']));
 						//echo $policyInfo[$header]['p_id'];
@@ -206,7 +206,7 @@
 							echo (int)$last_week . " " . (int)date("Ymd", strtotime($previousBreakdowns[$header]['c_timestamp']));
 							$i++;
 							?>
-							<a title="View Breakdown" <?php if(($previousBreakdowns[$header]['c_ul_id'] == $_SESSION['userID']) && ((int)date("Ymd", strtotime($previousBreakdowns[$header]['c_timestamp'])) > (int)$last_week)) {  ?> class="show-overlay" id="viewBD:<?php echo $previousBreakdowns[$header]['bd_id'] ?>" <?php } ?>style="color: #333; text-decoration:none;"><table width="100%" border="0">
+							<a title="View Breakdown" <?php if(($previousBreakdowns[$header]['c_ul_id'] == $_SESSION['userID']) && ((int)date("dmY", strtotime($previousBreakdowns[$header]['c_timestamp'])) > (int)$last_week)) {  ?> class="show-overlay" id="viewBD:<?php echo $previousBreakdowns[$header]['bd_id'] ?>" <?php } ?>style="color: #333; text-decoration:none;"><table width="100%" border="0">
 							  <tr>
 								<th width="40">#<?php echo $i ?></th>
 								<th>Date</th>
