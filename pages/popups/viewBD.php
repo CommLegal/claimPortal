a8e8f5a309b0c72d5f6ac9166c881d89a1e15003
unknown <js9@OC-WS-576-PC.onecall.local>
2015-12-03 14:23:17 +0000

    a

--- pages/popups/viewBD.php
+++ pages/popups/viewBD.php
@@ -4,50 +4,39 @@ if(!empty($_POST['callValues'])) {
 	require("../includes/mysqlwrapper_class.php");
 	$conn = new mysqlwrapper_class;
 	
-	$insertValues = $conn->execute_sql("select", array("*"), "third_party", "tp_id=?", array("i" => $_POST['callValues']));
+	$insertValues = $conn->execute_sql("select", array("*"), "breakdown_assistance", "bd_id=?", array("i" => $_POST['callValues']));
 }
 
 echo $_POST['callValues'];
 ?>
-<form action="" method="post" enctype="multipart/form-data" name="addTP" id="addTP">
-<div class="left"><label>Name:</label></div>
-<div class="right"><input name="tp_name" type="text" id="tp_name" size="30" placeholder="" value="<?php echo $insertValues[0]['tp_name'] ?>" required /></div>
+<form action="" method="post" enctype="multipart/form-data" name="viewBD" id="viewBD">
+    <div class="left"><label>Details of breakdown:</label></div>
+    <div class="right"><textarea type="text" name ="breakdown_assistance--bd_further_info" rows = "10" cols="70" class="form-control mb25" placeholder="" disabled> </textarea></div>
+    
+    <div class="left"><label>Status:</label></div>
+    <div class="right"><select id = "breakdown_assistance--bd_assisted_unassisted" name="breakdown_assistance--bd_assisted_unassisted">
+        <option value="">Select</option>
+        <option value="Assisted">Assisted</option>
+        <option value="Unassisted">Unassisted</option>
+    </select></div>
+    
+    <div class="left"><label>Reason for Unassisted (if applicable):</label></div>
+    <div class="right"><select id = "breakdown_assistance--bd_unassist_reason" name="breakdown_assistance--bd_unassist_reason">
+        <option value="0">Select</option>
+        <option value="1">Wrong fuel</option>
+        <option value="2">Tyre related on basic</option>
+        <option value="3">Key related</option>
+        <option value="4">Cancelled</option>
+        <option value="5">Same breakdown fault as previous</option>
+        <option value="Other">Other</option>
+    </select></div>
+    
+    <div class="left"><label>More Details:</label></div>
+    <div class="right"><textarea type="text" name ="breakdown_assistance--bd_unassist_desc" rows = "10" cols="70" class="form-control mb25" placeholder="" > </textarea></div>
 
-<div class="left"><label>Registration:</label></div>
-<div class="right"><input name="tp_v_reg" type="text" id="tp_v_reg" size="30" placeholder="" value="<?php echo $insertValues[0]['tp_v_reg'] ?>" required /></div>
-
-<div class="left"><label>Make & Model:</label></div>
-<div class="right"><input name="tp_make_model" type="text" id="tp_make_model" size="30" placeholder="" value="<?php echo $insertValues[0]['tp_make_model'] ?>" /></div>
-
-<div class="left"><label>Contact Number:</label></div>
-<div class="right"><input name="tp_contact_number" type="text" id="tp_contact_number" size="30" placeholder="" value="<?php echo $insertValues[0]['tp_contact_number'] ?>" required /></div>
-
-<div class="left"><label>Alternative Number:</label></div>
-<div class="right"><input name="tp_contact_alternative" type="text" id="tp_contact_alternative" size="30" placeholder="" value="<?php echo $insertValues[0]['tp_contact_alternative'] ?>"/></div>
-
-<div class="left"><label>TP Type: &nbsp;</label></div>
-<div class="right">
-<select name="tp_type" id ="tp_type">
-  <option selected="selected">Select</option>
-    <option value="Motorist"<?php echo (($insertValues[0]['tp_type'] == "Motorist") ? " selected=selected" : "") ?>>Motorist</option>
-    <option value="Motorcyclist"<?php echo (($insertValues[0]['tp_type'] == "Motorcyclist") ? " selected=selected" : "") ?>>Motorcyclist</option>
-    <option value="Cyclist"<?php echo (($insertValues[0]['tp_type'] == "Cyclist") ? " selected=selected" : "") ?>>Cyclist</option>
-    <option value="Pedestrian"<?php echo (($insertValues[0]['tp_type'] == "Pedestrian") ? " selected=selected" : "") ?>>Pedestrian</option>
-    <option value="Other"<?php echo (($insertValues[0]['tp_type'] == "Other") ? " selected=selected" : "") ?>>Other</option>
-</select>
-</div>
-
-<div class="left"><label>Remove: &nbsp;</label></div>
-<div class="right">
-  <input type="checkbox" name="tp_inactive" id="tp_inactive" value="Y"<?php echo (($insertValues[0]['tp_inactive']) ? " checked=checked" : "") ?> />
-</div>
-
-<input type="hidden" name="tp_c_id" id="tp_c_id" value="<?php echo $_SESSION['claimID'] ?>" />
-<input type="hidden" name="table" id="table" value="third_party" />
-<?php if(empty($_POST['callValues'])) { ?>
-<input type="hidden" name="db_callType" id="db_callType" value="insert" />
-<?php } else { ?>
-<input type="hidden" name="tp_id" id="tp_id" value="<?php echo $_POST['callValues'] ?>" />
+<input type="hidden" name="table" id="table" value="breakdown_assistance" />
+<?php if(!empty($_POST['callValues'])) { ?>
+<input type="hidden" name="bd_id" id="bd_id" value="<?php echo $_POST['callValues'] ?>" />
 <input type="hidden" name="db_callType" id="db_callType" value="update" />
 <?php } ?>
 <div class="left">&nbsp;</div><div class="right"><input type="submit" class = "btn btn-primary" name="save" id="save" value = "Save" /></div>
