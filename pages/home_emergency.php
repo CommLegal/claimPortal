f8c4850cc2ce78d59957ef1684c6f42d4124b69f
unknown <js9@OC-WS-576-PC.onecall.local>
2016-01-07 10:51:03 +0000

    a

--- pages/home_emergency.php
+++ pages/home_emergency.php
@@ -22,12 +22,16 @@
             
             <label>Reason for Unassisted:</label>
             <select id = "home_emergency--he_unassist_reason" name="home_emergency--he_unassist_reason">
-                <option value="1">Wrong fuel</option>
-                <option value="2">Tyre related on basic</option>
-                <option value="3">Key related</option>
-                <option value="4">Cancelled</option>
-                <option value="5">Same breakdown fault as previous</option>
-                <option value="O">Other</option>
+                <option value="1">Reoccurring issue</option>
+                <option value="2">Mechanical/Electrical Failure</option>
+                <option value="3">Accidental Damage</option>
+                <option value="4">Contents only cover</option>
+                <option value="5">More than 3 claims in one year</option>
+                <option value="6">Within 14 days</option>
+                <option value="7">Not informed within 24 hours</option>
+                <option value="8">Boilers</option>
+                <option value="9">Trace & Access</option>
+                <option value="10">Other – Out of Criteria</option>
             </select>
             
             <br /><br />
@@ -52,6 +56,25 @@
             <div id = "reasonbox">
                 <label>Details of emergency:</label>
                 <textarea type="text" name ="home_emergency--he_further_info" rows = "10" cols="70" class="form-control mb25" placeholder="" > </textarea>
+            </div>
+            
+            <label>Reason for Assisted:</label>
+            <select id = "home_emergency--he_unassist_reason" name="home_emergency--he_unassist_reason">
+                <option value="1">Burst/Blocked Pipes</option>
+                <option value="2">Fire</option>
+                <option value="3">Burglary/attempted burglary</option>
+                <option value="4">Storm damage</option>
+                <option value="5">Removal of wasps/hornet nests</option>
+                <option value="6">Damage to locks/Entrance</option>
+                <option value="7">Damage to bricks/doors/windows</option>
+                <option value="8">Guttering – Upgrade Only</option>
+            </select>
+     
+            <br /><br />
+            
+            <div id="reasonbox">
+                <label>More Details:</label>
+                <textarea type="text" name ="home_emergency--he_unassist_desc" rows = "10" cols="70" class="form-control mb25" placeholder="" > </textarea>
             </div>
         
         	<input type="hidden" id="claimType" name="claimType" value="home_emergency"  />
