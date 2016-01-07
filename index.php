3cafeafe1f57e20240d03ece1fcd1a34e75331a4
unknown <js9@OC-WS-576-PC.onecall.local>
2016-01-07 09:56:49 +0000

    a

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require("includes/config.php");
 require("includes/mysqlwrapper_class.php");
 $conn = new mysqlwrapper_class;
 
-$userLogin = $conn->execute_sql("select", array("ul_id", "ul_username", "ul_forename", "ul_surname", "ul_company_title", "ul_breakdown", "ul_accident_recovery", "ul_fnol", "ul_windscreen"), "user_login", "ul_username=LOWER(?)", array("s" => strtolower($_SESSION['userName'])));
+$userLogin = $conn->execute_sql("select", array("ul_id", "ul_username", "ul_forename", "ul_surname", "ul_company_title", "ul_breakdown", "ul_accident_recovery", "ul_fnol", "ul_windscreen", "ul_home_emergency", "ul_household_claim"), "user_login", "ul_username=LOWER(?)", array("s" => strtolower($_SESSION['userName'])));
 
 $_SESSION['userName'] = $userLogin[0]['ul_username'];
 $_SESSION['userID'] = $userLogin[0]['ul_id'];
