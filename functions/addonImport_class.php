60763cac904ad81f75e49873022a5f1a3746df23
unknown <js9@OC-WS-576-PC.onecall.local>
2016-01-12 12:08:19 +0000

    a

--- functions/addonImport_class.php
+++ functions/addonImport_class.php
@@ -181,8 +181,9 @@ class addonImport_class {
 			
 			if($row['addons']['trans_type'] == "Cancellation") {
 				$policyData['a_cancel_date'] = date("Y-m-d");
-				unset($row['addons']['trans_type']);
+				
 			}
+			unset($row['addons']['trans_type']);
 			
 			$a_id = $this->checkKey("a_id", "addons", "a_policy_number = '" . $policyNumber . "' and a_scheme = '" . $policyData['a_scheme'] . "'");
 			
