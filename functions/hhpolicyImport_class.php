d972a21e823fe1dcf9c2f25237d25a162e0cf680
unknown <js9@OC-WS-576-PC.onecall.local>
2016-01-14 12:06:08 +0000

    a

--- functions/hhpolicyImport_class.php
+++ functions/hhpolicyImport_class.php
@@ -453,6 +453,7 @@ class hhpolicyImport_class {
 			else {
 				unset($result);
 				if($foebis->error) {
+					echo $foebis->error;
 					$this->writeErrorLog($foebis->error, $query);
 					//return json_encode(array('success'=>'false'));
 				}
