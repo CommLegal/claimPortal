189e3b7e4a3676d3090714c8fecaf51a7ebe2637
unknown <js9@OC-WS-576-PC.onecall.local>
2016-01-18 12:08:20 +0000

    a

--- pages/accident_recovery_upload.php
+++ pages/accident_recovery_upload.php
@@ -5,7 +5,7 @@ if(empty($_SESSION['claimID'])) {
 	
 	$insertValues = $conn->execute_sql("insert", array("c_claim_type" => $_POST['claimType'], "c_timestamp" => $_POST['claimTimestamp'], "c_p_id" => $_POST['policyId'], "c_ph_id" => $_POST['policyHolderId'], "c_ul_id" => $_SESSION['userID']), "claims", "", "");
 	
-	$_SESSION['claimID'] = $insertValues;
+	$_SESSION['claimID'] = trim($insertValues);
 	echo trim($insertValues);
 }
 else {
