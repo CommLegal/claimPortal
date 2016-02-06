a14d95df53f0c05697278f048ae08efeccf5bf02
unknown <js9@OC-WS-576-PC.onecall.local>
2016-02-06 05:55:14 +0000

    a

--- pages/fnol.php
+++ pages/fnol.php
@@ -46,8 +46,6 @@
 					$open = strtotime(date("Y-m-d 09:00:00"));
 					$close = strtotime(date("Y-m-d 17:30:00"));
 					
-					echo $time . " - " . $open . " : " . $close;
-					
 					if($time >= $open && $time <= $close) { 
 						if($policyInfo[$header]['p_broker'] !== "ONE Insurance Limited") {
 							echo "<div class=\"well\"><h3>Within Normal Operating Hours</h3>Please inform the customer that they need to contact their insurance company 'Commercial Legal' and they will assist.<h3>Please call 0203 738 7300</h3></div>";
