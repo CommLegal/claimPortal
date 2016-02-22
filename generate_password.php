a1625b473cec5935e49d2bfeab82266792d9926b
unknown <js9@OC-WS-576-PC.onecall.local>
2016-02-22 10:52:55 +0000

    a

--- generate_password.php
+++ generate_password.php
@@ -1,5 +1,5 @@
 <?php
-$password = "password12345";
+$password = "SLh?6X";
 $encryptKey = "rE!wec#PpcnDK7*&5S#V87kRc69G2zVe";
 $encrypted = md5($password . $encryptKey . $password . $encryptKey . $password . $encryptKey);
 echo $encrypted;
