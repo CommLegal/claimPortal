e65c3ffc230b2fd305fde78ee9a912f7533c41c0
Jonathan Sutton <js9@onecall.local>
2016-03-04 12:18:26 +0000

    a

--- pages/household_claim.php
+++ pages/household_claim.php
@@ -15,7 +15,7 @@
     
     <div class = "hcDisplay" id = "hcDisplay" style="display:none"><!-- FNOL UNASSISTED -->	
     	<form method="post" enctype="multipart/form-data" id="hc_form" data-toggle="validator" role="form">
-            
+            <div class="form-group">
             <label>Date of Claim:</label>
             <div class="input-group mb10">
             <input required type="text" name="household_claim--hc_date" id="household_claim--hc_date" class="form-control mb10 datepicker" placeholder="" value="">
@@ -25,7 +25,7 @@
             <br />
                 
             <label>Claim Type:</label>
-            <select id = "household_claim--hc_claim_type" name="household_claim--hc_claim_type">
+            <select id = "household_claim--hc_claim_type" name="household_claim--hc_claim_type" class="form-control">
                 <option value="1">Accidental Damage</option>
                 <option value="2">Accidental Loss</option>
                 <option value="3">Escape of Water</option>
@@ -41,14 +41,14 @@
         
             <div id="reasonbox">
                 <label>More Details:</label>
-                <textarea required type="text" name ="household_claim--hc_desc" rows = "10" cols="70" class="form-control mb25" placeholder="" > </textarea>
+                <textarea type="text" name ="household_claim--hc_desc" rows = "10" cols="70" class="form-control mb25" placeholder="" required> </textarea>
             </div>
         
         	<input type="hidden" id="claimType" name="claimType" value="household_claim"  />
             
             <button class = "btn btn-lg btn-success mb25" id ="hc_submit">Accept & Save &nbsp;<i class="fa fa-lg fa-floppy-o"></i></button>
             <button class = "btn btn-lg btn-danger mb25 cancel pull-right">Cancel &nbsp;<i class="fa fa-lg fa-times"></i></button>
-            
+            </div>
         </form>
     </div>
         
