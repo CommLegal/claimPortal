28e5ae468558b4dcc647bc0fdb6f8083480e60d1
Jonathan Sutton <js9@onecall.local>
2016-04-06 17:11:03 +0100

    a

--- generate_password.php
+++ generate_password.php
@@ -1,5 +1,5 @@
 <?php
-$password = "SLh?6X";
+$password = "NationW1d3";
 $encryptKey = "rE!wec#PpcnDK7*&5S#V87kRc69G2zVe";
 $encrypted = md5($password . $encryptKey . $password . $encryptKey . $password . $encryptKey);
 echo $encrypted;
