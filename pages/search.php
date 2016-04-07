0598b610052673a2871a6461a82081a998ba7add
Jonathan Sutton <js9@onecall.local>
2016-04-07 16:12:40 +0100

    a

--- pages/search.php
+++ pages/search.php
@@ -83,6 +83,12 @@ if (!empty($_SESSION['claimID'])) {
                 $interval = date_diff(date_create($policyDetail[0]['p_inception_date']), date_create($policyDetail[0]['p_renewal_date']));
 
                 echo $interval->format("%a");
+                if($interval->format("%a") < 370) {
+                    $policyDetail[0]['p_policy_newrnwl'] = "New Policy";
+                }
+                else {
+                    $policyDetail[0]['p_policy_newrnwl'] = "Renewed Policy";
+                }
                 //$policyDetail[0]['p_policy_newrnwl'] = 
                 
                 
